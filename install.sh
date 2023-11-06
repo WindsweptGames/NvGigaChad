7c4e8f86fb5f0e25b9fa8b7bcdb1cfc7474dee2f
Garrett McCormack <awindsweptemu@proton.me>
2023-11-06 16:27:07 -0600

    adding colors to terminal messages

--- install.sh
+++ install.sh
@@ -2,12 +2,28 @@
 
 INSTALL_DIR=$HOME
 TAR_DIR="nvim-linux64"
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+red=$(tput setaf 1)
+normal=$(tput sgr0)
+
+function printgln () {
+  printf '%s%s%s\n' "${green}" "$1" "${normal}"
+}
+
+function printyln () {
+  printf '%s%s%s\n' "${yellow}" "$1" "${normal}"
+}
+
+function printrln () {
+  printf '%s%s%s\n' "${red}" "$1" "${normal}"
+}
 
 if [ $# -eq 0 ]; then
-    printf 'No installation directory provided, installing to %s\n' "$INSTALL_DIR"
+    printyln "No installation directory provided, installing to $INSTALL_DIR"
   else
     INSTALL_DIR=$1
-    printf 'Installing to %s\n' "$INSTALL_DIR"
+    printgln "Installing to $INSTALL_DIR"
 fi
 
 function yes_or_no () {
@@ -21,28 +37,28 @@ function yes_or_no () {
 }
 
 function install () {
-  printf "Fetching Neovim..."
+  printgln 'Fetching Neovim...'
   mkdir -p "$INSTALL_DIR"
   curl -L -o "$INSTALL_DIR/nvim.tar.gz" https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
   tar -zxf "$INSTALL_DIR/nvim.tar.gz" -C "$INSTALL_DIR"
   rm "$INSTALL_DIR/nvim.tar.gz"
   cmd="$INSTALL_DIR/$TAR_DIR/bin/nvim --version"
   check=$(eval "$cmd" | awk '{print $1;}' | head -n 1)
-  printf "Updating config at %s/.config/nvim\n" "$HOME"
+  printgln "Updating config at $HOME/.config/nvim"
   if [ "$check" = "NVIM" ]; then
     #install was successful so lets add the config
     rm -rf "$HOME/.config/nvim"
     git clone https://github.com/NvChad/NvChad "$HOME/.config/nvim" --depth 1
     git clone https://github.com/aWindsweptEmu/nvchad-custom "$HOME/.config/nvim/lua/custom" --depth 1
   else
-    printf "Could not verify nvim, exiting..."
+    printyln "Could not verify nvim, exiting..."
     exit 1
   fi
-  printf "Make sure to add Neovim to your path: export PATH=\$PATH:%s/bin\n" "$INSTALL_DIR/$TAR_DIR"
+  printgln "Make sure to add Neovim to your path: export PATH=\$PATH:$INSTALL_DIR/$TAR_DIR/bin"
 }
 
 if [ -d "$INSTALL_DIR/$TAR_DIR" ]; then
-  if yes_or_no "$INSTALL_DIR/$TAR_DIR already exists. Would you like to reinstall? This will remove your current installation and configuration!"; then
+  if yes_or_no "$INSTALL_DIR/$TAR_DIR already exists. Would you like to reinstall? ${red}This will remove your current installation and configuration!${normal}"; then
     rm -rf "${INSTALL_DIR:?$HOME}/$TAR_DIR"
     install
   fi 
