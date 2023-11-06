0f86e73835feb5b5cb7e00d5bb09d6e79d4f776e
Garrett McCormack <awindsweptemu@proton.me>
2023-11-06 15:51:05 -0600

    updating script to provide more informative output

--- install.sh
+++ install.sh
@@ -21,23 +21,28 @@ function yes_or_no () {
 }
 
 function install () {
+  printf "Fetching Neovim..."
   mkdir -p "$INSTALL_DIR"
   curl -L -o "$INSTALL_DIR/nvim.tar.gz" https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
   tar -zxf "$INSTALL_DIR/nvim.tar.gz" -C "$INSTALL_DIR"
   rm "$INSTALL_DIR/nvim.tar.gz"
   cmd="$INSTALL_DIR/$TAR_DIR/bin/nvim --version"
   check=$(eval "$cmd" | awk '{print $1;}' | head -n 1)
+  printf "Updating config at %s/.config/nvim\n" "$HOME"
   if [ "$check" = "NVIM" ]; then
     #install was successful so lets add the config
     rm -rf "$HOME/.config/nvim"
     git clone https://github.com/NvChad/NvChad "$HOME/.config/nvim" --depth 1
     git clone https://github.com/aWindsweptEmu/nvchad-custom "$HOME/.config/nvim/lua/custom" --depth 1
+  else
+    printf "Could not verify nvim, exiting..."
+    exit 1
   fi
-  echo "Make sure to add Neovim to your path: export PATH=$PATH:$INSTALL_DIR/$TAR_DIR/bin"
+  printf "Make sure to add Neovim to your path: export PATH=\$PATH:%s/bin\n" "$INSTALL_DIR/$TAR_DIR"
 }
 
 if [ -d "$INSTALL_DIR/$TAR_DIR" ]; then
-  if yes_or_no "$INSTALL_DIR/$TAR_DIR already exists. Would you like to replace its contents?\n"; then
+  if yes_or_no "$INSTALL_DIR/$TAR_DIR already exists. Would you like to reinstall? This will remove your current installation and configuration!\n"; then
     rm -rf "${INSTALL_DIR:?$HOME}/$TAR_DIR"
     install
   fi 
